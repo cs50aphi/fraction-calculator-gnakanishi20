4e905e8b3ac28fb929826731c518b9ce428f7395
gnakanishi20 <gnakanishi20@punahou.edu>
2020-05-01 06:53:33 +0000

    started much of the code but didn't fully read the spec so coded a lot out of the methods

--- FractionDriver.java
+++ FractionDriver.java
@@ -5,20 +5,66 @@ public class FractionDriver
     public static void main(String [] args)
     {
         Scanner kb = new Scanner(System.in);
-        System.out.print("Num1: ");
-        int n1 = kb.nextInt();
-        System.out.print("Den1: ");
-        int d1 = kb.nextInt();
-        System.out.print("Num2: ");
-        int n2 = kb.nextInt();
-        System.out.print("Den2: ");
-        int d2 = kb.nextInt();
-        Fraction f1 = new Fraction(n1, d1);
-        Fraction f2 = new Fraction(n2, d2);
-        System.out.println(f1 + " + " + f2 + " is " + f1.add(f2));
-        System.out.println(f1 + " - " + f2 + " is " + f1.subtract(f2));
-        System.out.println(f1 + " * " + f2 + " is " + f1.multiply(f2));
-        System.out.println(f1 + " / " + f2 + " is " + f1.divide(f2));
-        System.out.println(f1 + " is the same as " + f2 + ": " + f1.equals(f2));
+        System.out.print("Please enter an operation (+, -, *, /, =, or Q to Quit):");
+        String operation = getOperation(kb.nextLine());
+        System.out.println("Please enter a fraction (a/b) or interger (a):");
+        String fraction = kb.nextLine();
+        String []hold = fraction.split("/");
+        Fraction f1 = new Fraction(hold[0], hold[1]);
+        System.out.println("Please enter a fraction (a/b) or interger (a):");
+        String fraction1 = kb.nextLine();
+        String []hold1 = fraction1.split("/");
+        Fraction f2 = new Fraction(hold1[0], hold1[1]);
+        switch (operation)
+        {
+            case "+":
+            {
+                System.out.println(f1 + " + " + f2 + " is " + f1.add(f2));
+            }
+            
+            case "-":
+            {
+                System.out.println(f1 + " - " + f2 + " is " + f1.subtract(f2));
+            }
+            
+            case "*":
+            {
+                System.out.println(f1 + " * " + f2 + " is " + f1.multiply(f2));
+            }
+            
+            case "/":
+            {
+                System.out.println(f1 + " / " + f2 + " is " + f1.divide(f2));
+            }
+            
+            case "Q":
+            case "q":
+            {
+                System.out.println("Goodbye!");
+            }
+        }
+    }
+    
+    public static String getOperation(String opper)
+    {
+        String input = "+, -, /, *, q, Q, =";
+        while(!input.contains(opper))
+            {
+                System.out.println("Invalid input (+, -, *, /, =, or Q to Quit):");
+                // String inputRow = kb.nextLine();
+                opper = kb.nextLine();
+            }
+        
+        return opper;
+    }
+    
+    public static boolean validFraction(String fraction)
+    {
+        
+    }
+    
+    public static Fraction getFraction(String frac)
+    {
+        
     }
 }

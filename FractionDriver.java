758b34e089aed205da13a424670767b667fb6996
gnakanishi20 <gnakanishi20@punahou.edu>
2020-05-02 01:41:50 +0000

    my valid fraction is not working but the sytax and printing out the equation should be good.

--- FractionDriver.java
+++ FractionDriver.java
@@ -4,67 +4,100 @@ public class FractionDriver
 {
     public static void main(String [] args)
     {
+        boolean stop = false;
         Scanner kb = new Scanner(System.in);
-        System.out.print("Please enter an operation (+, -, *, /, =, or Q to Quit):");
-        String operation = getOperation(kb.nextLine());
-        System.out.println("Please enter a fraction (a/b) or interger (a):");
-        String fraction = kb.nextLine();
-        String []hold = fraction.split("/");
-        Fraction f1 = new Fraction(hold[0], hold[1]);
-        System.out.println("Please enter a fraction (a/b) or interger (a):");
-        String fraction1 = kb.nextLine();
-        String []hold1 = fraction1.split("/");
-        Fraction f2 = new Fraction(hold1[0], hold1[1]);
-        switch (operation)
+        while(!stop)
         {
-            case "+":
+            String opperation = getOperation(kb);
+            Fraction f1 = getFraction(kb);
+            Fraction f2 = getFraction(kb);
+            
+            if(opperation.equals("+"))
             {
                 System.out.println(f1 + " + " + f2 + " is " + f1.add(f2));
             }
             
-            case "-":
+            if(opperation.equals("-"))
             {
                 System.out.println(f1 + " - " + f2 + " is " + f1.subtract(f2));
             }
             
-            case "*":
+            if(opperation.equals("*"))
             {
                 System.out.println(f1 + " * " + f2 + " is " + f1.multiply(f2));
             }
             
-            case "/":
+            if(opperation.equals("/"))
             {
                 System.out.println(f1 + " / " + f2 + " is " + f1.divide(f2));
             }
             
-            case "Q":
-            case "q":
+            if(opperation.equals("q") || opperation.equals("Q"))
             {
                 System.out.println("Goodbye!");
+                stop = true;
             }
         }
     }
     
-    public static String getOperation(String opper)
+    public static String getOperation(Scanner kb)
     {
+        System.out.print("Please enter an operation (+, -, *, /, =, or Q to Quit):");
+        String operation = kb.nextLine();
         String input = "+, -, /, *, q, Q, =";
-        while(!input.contains(opper))
+        while(!input.contains(operation))
             {
                 System.out.println("Invalid input (+, -, *, /, =, or Q to Quit):");
                 // String inputRow = kb.nextLine();
-                opper = kb.nextLine();
+                operation = kb.nextLine();
             }
         
-        return opper;
+        return operation;
     }
     
     public static boolean validFraction(String fraction)
     {
-        
+        if(fraction.substring(1, fraction.length()).contains("-"))
+        {
+            return false;
+        }
+        String remove = fraction.replaceAll("[0-9]", "");
+        if(remove.equals("/") || remove.isEmpty())
+        {
+            return true;
+        }
+        return false;
     }
     
-    public static Fraction getFraction(String frac)
+    public static Fraction getFraction(Scanner kb)
     {
-        
+        System.out.println("Please enter a fraction (a/b) or interger (a):");
+        String frac = kb.nextLine();
+        boolean valid = validFraction(frac);
+        do
+        {
+            System.out.println("Invalid fraction. Please enter (a/b) or (a) where a and b are integers and b is not zero:");
+            frac = kb.nextLine();
+            valid = validFraction(frac);
+        }
+        while(valid = false);
+        int counter = 0;
+        if(frac.contains("/"))
+        {
+            do
+            {
+                counter++;
+            }
+            while(!frac.substring(counter, counter + 1).equals("/"));
+            String num = frac.substring(0, counter);
+            String denom = frac.substring(counter + 1, frac.length());
+            return new Fraction(Integer.parseInt(num), Integer.parseInt(denom));
+
+        }
+        else
+        {
+            String num = frac;
+            return new Fraction(Integer.parseInt(num));
+        }
     }
 }
